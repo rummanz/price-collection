0bc214460ff0aa672436b0150e628d4cd5aa5c2b
Rumman Zaman <rummanzaman11@gmail.com>
2025-02-01 22:20:30 +0100

    Update rank_email.py

--- rank_email.py
+++ rank_email.py
@@ -8,14 +8,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Database connection parameters
+# Database connection parameters from .env
 config = {
-    'user': 'pyuser11',
-    'password': 'P7Oc19B5mTAmb2Kh',
-    'host': '127.0.0.1',
-    'database': 'products',
+    'user': os.getenv("DB_USER"),
+    'password': os.getenv("DB_PASSWORD"),
+    'host': os.getenv("DB_HOST"),
+    'database': os.getenv("DB_NAME"),
     'raise_on_warnings': True
-
 }
 
 def load_email_template():
